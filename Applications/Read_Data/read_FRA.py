57474966eb03cfa3e1fa941164653e6f6b5e8628
Christian.Morland <christian.morland@thuenen.de>
2025-01-28 16:11:31 +0100

    add rudimentary read_FRA.py

---
+++ Applications/Read_Data/read_FRA.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import numpy as np
+from  tqdm import tqdm
+import os
+import time
+from itertools import product
+import matplotlib.pyplot as plt
+from enum import Enum
+import pyarrow
+import fastparquet
+
+def read_original_data():
+    try:
+        pass
+        #data = dt.fread("FRA_Data\\FRA_Years_2023_01_27.csv").to_pandas()
+    except FileNotFoundError:
+        print("No data found in directory! Please download data bulk from FAOSTat to this directoy: " + os.path.abspath(os.getcwd()))
+    return data
+
+def ResultWriter(data_all: pd.DataFrame):
+    data_all.to_parquet("FRA_Data\\FRA_Data.parquet")
+
+data = read_original_data()
+ResultWriter(data)
+data_wood = pd.read_parquet("FRA_Data\\FRA_Data.parquet")
+print(data_wood)
