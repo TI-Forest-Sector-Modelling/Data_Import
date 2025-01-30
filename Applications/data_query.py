fd455b0c91cfc1ffe1eb6019ab0f6605d0d4841e
Christian.Morland <christian.morland@thuenen.de>
2025-01-30 09:51:58 +0100

    add fao element codes to Dictionaries

--- Applications/data_query.py
+++ Applications/data_query.py
@@ -12,5 +12,16 @@ file_list = ["BACI_DATA_as_vector.parquet",
              "WDI_DATA_as_vector.parquet"]
 
 data_dict = data_import(file_list=file_list)
-print(data_dict)
+
+fao_data = data_dict["FAO_data"]
+print(fao_data.Element_Code.unique())
+# def read_add_info(self, country_file_name:str="Country_Master"):
+#     country_file= self.add_input_path / f"{country_file_name}.csv"
+#     self.add_country_info = pm.read_original_data(input_path=country_file)
+#     self.add_country_info = self.add_country_info[["FAO Code","ISO-Code"]]
+#     self.country_dict = dict(zip(self.add_country_info["FAO Code"], self.add_country_info["ISO-Code"]))
+
+# def iso_codes_to_data(self) -> pd.DataFrame:
+#     print(self.data)
+#     self.data['Area_Code'] = self.data['Area_Code'].map(self.country_dict).fillna(self.data['Area_Code'])
 
---
+++ Input/Dictionaries/fao_codes.py
@@ -0,0 +1,10 @@
+element_dict={
+    5622:"Import Value (in 1000 USD)",
+    5922:"Export Value (in 1000 USD)",
+    5516:"Production (in m3)",
+    5616:"Import Quantity (in m3)",
+    5916:"Export Quantity (in m3)",
+    5510:"Production (in t)",
+    5610:"Import Quantity (in t)",
+    5910:"Export Quantity (in t)"
+}
